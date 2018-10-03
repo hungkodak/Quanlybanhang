f46fdd149b2f947d8b12cca9a3ba88f13ba69e38
hungkodak <hung.truonglap@gmail.com>
2018-10-03 10:20:44 +0700

    update

--- sql/02_Create_Table_User_Management.sql
+++ sql/02_Create_Table_User_Management.sql
@@ -1,4 +1,4 @@
 use usermanagement;
 
-create table accounts (username VARCHAR(20), name Varchar(20), password VARCHAR(20), role int, lastupdate Datetime,PRIMARY KEY (username))
+create table accounts (username VARCHAR(50), name Varchar(50), password VARCHAR(50), role int, lastupdate Datetime,PRIMARY KEY (username))
 
--- sql/03_Query_user_authen.sql
+++ sql/03_Query_user_authen.sql
@@ -2,7 +2,7 @@ SELECT * FROM usermanagement.accounts;
 
 SELECT * FROM usermanagement.accounts where username = 'hungkodak' and password = MD5('hungkodak') limit 1;
 
-INSERT INTO accounts(name,username,password,role,lastupdate) VALUES ('Tào Hùng','hungkodak',MD5('hungkodak'),0,date(now()));
+INSERT INTO accounts(name,username,password,role,lastupdate) VALUES ('Truong Lap Hung','hungkodak',MD5('hungkodak'),0,date(now()));
 
 DELETE FROM `usermanagement`.`accounts` WHERE (`username` = 'hungkodak');
 
